c4cb859f2a00f831f6db301b1f281f4aa44e6e7c
Vipul Sharma <vipul.sharma20@gmail.com>
2021-08-30 20:26:37 +0530

    add: request SLU service using CSV dataframes

--- trail/handler.go
+++ trail/handler.go
@@ -1,27 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
+	"encoding/json"
+	"log"
+	"os"
 	"sync"
 
 	"github.com/spf13/cobra"
 )
 
+var (
+	wg sync.WaitGroup
+)
+
+func panicHandler() {
+	if err := recover(); err != nil {
+		log.Println("Could not get predictions:", err)
+	}
+}
+
+func writeOutput(src chan SLUResponse) {
+	fp, err := os.OpenFile(outputCsv, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	writer := csv.NewWriter(fp)
+	defer writer.Flush()
+	defer fp.Close()
+
+	for msg := range src {
+		entities, err := json.Marshal(msg.Response.Entities)
+		intents, err := json.Marshal(msg.Response.Intents)
+		if err != nil {
+			log.Fatalln("Could not Marshal ", err)
+		}
+		record := []string{msg.Uuid, string(entities), string(intents)}
+		if err := writer.Write(record); err != nil {
+			log.Fatalln("Error writing record to file", err)
+		}
+	}
+}
+
 // rootHandler - follow command handler
 func rootHandler(cmd *cobra.Command, args []string) {
-	reader := Reader{path: inputProto}
-	record := reader.yamlReader()
+	defer panicHandler()
+	reader := Reader{path: inputCsv}
+	record := reader.csvReader()
+	sluClient := NewSLUClient(sluHost)
+	outputChannel := make(chan SLUResponse)
 
-	// log record to stdout
-	for idx, element := range record.Calls {
-		fmt.Println(idx, element.Id)
+	log.Println("Making requests to SLU service")
+	maxGoroutines := 10
+	guard := make(chan struct{}, maxGoroutines)
+
+	for _, item := range record.RequestBody {
+		wg.Add(1)
+		guard <- struct{}{} // would block if guard channel is already filled
+		go func(outputChannel chan SLUResponse, item SLURequestBody) {
+			go sluClient.Predict(outputChannel, item)
+			<-guard
+		}(outputChannel, item)
+		// go sluClient.Predict(outputChannel, item)
 	}
 
-	// jsonMap := make(map[string]interface{})
+	go func() {
+		wg.Wait()
+		close(outputChannel)
+	}()
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	// go Predict(jsonMap)
-	go runFlowTest(record)
-	wg.Wait()
+	writeOutput(outputChannel)
+	log.Printf("Predictions exported at: %s", outputCsv)
 }
--- trail/reader.go
+++ trail/reader.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"encoding/csv"
-	"google.golang.org/protobuf/proto"
+	"encoding/json"
+	"io"
+
+	// "encoding/json"
+
 	"io/ioutil"
 	"log"
 	"os"
 	pb "trail/trail/protos"
+
+	"google.golang.org/protobuf/proto"
 	// fsm "***REMOVED***/services"
 )
 
 type Reader struct {
 	path string
+}
+
+type SLURequestBody struct {
+	Text         string        `json:"text"`
+	Alternatives []interface{} `json:"alternatives"`
+	Context      struct {
+		CallUuid     string `json:"call_uuid"`
+		CurrentState string `json:"current_state"`
+		Uuid         string `json:"uuid"`
+	} `json:"context"`
+}
+
+type ListRequests struct {
+	RequestBody []SLURequestBody
 }
 
 func (reader Reader) yamlReader() *pb.CallDataFrame {
@@ -26,10 +46,9 @@ func (reader Reader) yamlReader() *pb.CallDataFrame {
 	}
 
 	return record
-
 }
 
-func (reader Reader) csvReader() [][]string {
+func (reader Reader) csvReader() ListRequests {
 	f, err := os.Open(reader.path)
 	if err != nil {
 		log.Fatal("Unable to read input file "+reader.path, err)
@@ -37,10 +56,37 @@ func (reader Reader) csvReader() [][]string {
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
-	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+reader.path, err)
-	}
+	requests := ListRequests{}
+	rowIdx := 0
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if rowIdx == 0 {
+			rowIdx++
+			continue
+		}
 
-	return records
+		sluRequest := SLURequestBody{}
+
+		// Get alternatives
+		var data []interface{}
+		err = json.Unmarshal([]byte(record[2]), &data)
+		if err != nil {
+			log.Fatal("error happened", err)
+		}
+		sluRequest.Alternatives = data
+
+		// Context
+		sluRequest.Context.CallUuid = record[0]
+		sluRequest.Context.CurrentState = record[6]
+		sluRequest.Context.Uuid = record[1]
+
+		requests.RequestBody = append(requests.RequestBody, sluRequest)
+	}
+	return requests
 }
