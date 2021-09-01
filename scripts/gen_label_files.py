6daf1c8a67a8441ba07380dd0298165aa7d6f656
Vipul Sharma <vipul.sharma20@gmail.com>
2021-09-01 18:33:12 +0530

    update: python script to generate label files

--- scripts/gen_label_files.py
+++ scripts/gen_label_files.py
@@ -5,15 +5,39 @@ import sys
 import json
 
 
+def _generate_intent_labels(intents):
+    intent = max(intents, key=lambda x: x["score"])
+    return intent["name"]
+
+
+def _generate_entity_labels(entities):
+    entity_list = []
+    for entity in entities:
+        entity_list.append(dict(
+            text=entity.get("body"),
+            type=entity.get("type"),
+            score=entity.get("score"),
+            value=entity.get("value"),
+        ))
+
+    return json.dumps(entity_list)
+
+
 def get_labeled_items(data, type_):
     records = filter(lambda d: d["response"].get(type_), data)
+
     with open(f"{type_}_label.csv", "w") as fp:
         writer = csv.writer(fp)
         for record in records:
-            writer.writerow([record["uuid"], json.dumps(record["response"][type_])])
+            writer.writerow([record["uuid"], label_func.get(record["response"][type_])])
 
 
 if __name__ == "__main__":
     data = json.loads(sys.stdin.read())
+    label_func = {
+        "intents": _generate_intent_labels,
+        "entities": _generate_entity_labels
+    }
+
     get_labeled_items(data, type_="intents")
     get_labeled_items(data, type_="entities")
